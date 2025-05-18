6da27cf6272d6325a35d5d46774a387b002c7e73
Paulo <pvvtoledo@gmail.com>
2025-05-18 17:16:06 -0300

    formulário implementado

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 
 import { router } from "./Routes";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | Pizza Shop" /> {/* %s recebe um título variável de acordo com a página */}
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  );
 }
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,3 +1,51 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
+import { Helmet } from "react-helmet-async";
+import {useForm} from 'react-hook-form'
+import * as z from 'zod'
+
+const signInForm = z.object({
+  email: z.string().email()
+})
+
+type SignInForm = z.infer<typeof signInForm>
+
 export function SignIn() {
-  return <h1>Signin</h1>;
+
+  const {register, handleSubmit, formState: {isSubmitting}} = useForm<SignInForm>()
+
+  async function handleSignIn (data: SignInForm) {
+    console.log(data)
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+  }
+
+  return (
+    <>
+      <Helmet title="Login" />
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            ]{/* tracking-tight diminui um pouco o letter spacing */}
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <p className="text-muted-foreground text-sm">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
+
+            {/* space-y controla margem no eixo y */}
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" {...register('email')}/>
+            </div>
+
+            <Button disabled={isSubmitting} className="w-full" type="submit">Acessar painel</Button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
