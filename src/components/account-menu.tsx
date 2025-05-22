f62c0fb2e69ef2608b35190b816210ffd75f2326
Paulo <pvvtoledo@gmail.com>
2025-05-22 12:08:58 -0300

    funcionalidade de logout implementada

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -8,27 +8,39 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { getProfile } from "@/api/get-profile";
 import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 import { Skeleton } from "./ui/skeleton";
 import { Dialog, DialogTrigger } from "./ui/dialog";
 import { StoreProfileDialog } from "./store-profile-dialog";
+import { signOut } from "@/api/sign-out";
+import { useNavigate } from "react-router-dom";
 
 export function AccountMenu() {
+  const navigate = useNavigate();
+
   //Parecido com o useMutation, mas usado para quando não há mutação (requisições GET, por exemplo). queryKey serve para identificar de maneira única essa requisição para que se algum componente repetir, não precisar executar novamente, pois guarda as informações da query no cache
   const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ["profile"],
     queryFn: getProfile,
-    staleTime: Infinity //Para nunca recarregar as informações novamente ao perder o foco da janela e voltar
+    staleTime: Infinity, //Para nunca recarregar as informações novamente ao perder o foco da janela e voltar
   });
 
   const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
     useQuery({
       queryKey: ["managed-restaurant"],
       queryFn: getManagedRestaurant,
-      staleTime: Infinity //Para nunca recarregar as informações novamente ao perder o foco da janela e voltar
+      staleTime: Infinity, //Para nunca recarregar as informações novamente ao perder o foco da janela e voltar
     });
+
+  const { mutateAsync: signOutFn, isPending: isSigningOut } = useMutation({
+    mutationFn: signOut,
+    //Executa se o signOut for bem sucedido. Replace: true garante que o usuário não valte para dentro do dashboard simplesmente apertando o botão de voltar do navegador
+    onSuccess: () => {
+      navigate("/sign-in", { replace: true });
+    },
+  });
 
   return (
     <Dialog>
@@ -71,9 +83,15 @@ export function AccountMenu() {
             </DropdownMenuItem>
           </DialogTrigger>
 
-          <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-            <LogOut className="mr-2 h-4 w-4 text-rose-500 dark:text-rose-400" />
-            <span>Sair</span>
+          <DropdownMenuItem
+            className="text-rose-500 dark:text-rose-400"
+            asChild
+            disabled={isSigningOut}
+          >
+            <button className="w-full" onClick={() => signOutFn()}>
+              <LogOut className="mr-2 h-4 w-4 text-rose-500 dark:text-rose-400" />
+              <span>Sair</span>
+            </button>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
