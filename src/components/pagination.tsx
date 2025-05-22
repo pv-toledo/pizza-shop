34fce42f7b75cfb491f97f248d1066c4ac9c7350
Paulo <pvvtoledo@gmail.com>
2025-05-22 15:14:47 -0300

    paginação implementada

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,16 +1,23 @@
-import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from "lucide-react";
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+} from "lucide-react";
 import { Button } from "./ui/button";
 
 export interface PaginationProps {
   pageIndex: number;
   totalCount: number;
   perPage: number;
+  onPageChange: (pageIndex: number) => Promise<void> | void;
 }
 
 export function Pagination({
   pageIndex,
   totalCount,
   perPage,
+  onPageChange,
 }: PaginationProps) {
   const pages = Math.ceil(totalCount / perPage) || 1; //Arredonda pra cima
   return (
@@ -20,26 +27,48 @@ export function Pagination({
       </span>
 
       <div className="flex items-center gap-6 lg:gap-7">
-        <div className="text-sm font-medium">Página {pageIndex + 1} de {pages}</div>
+        <div className="text-sm font-medium">
+          Página {pageIndex + 1} de {pages}
+        </div>
         <div className="flex items-center gap-2">
-            <Button variant="outline" className="h-8 w-8 p-0">
-                <ChevronsLeft className="h-4 w-4" />
-                <span className="sr-only">Primeira página</span>
-            </Button>
-            <Button variant="outline" className="h-8 w-8 p-0">
-                <ChevronLeft className="h-4 w-4" />
-                <span className="sr-only">Página anterior</span>
-            </Button>
+          <Button
+            onClick={() => onPageChange(0)}
+            variant="outline"
+            className="h-8 w-8 p-0"
+            disabled={pageIndex === 0}
+          >
+            <ChevronsLeft className="h-4 w-4" />
+            <span className="sr-only">Primeira página</span>
+          </Button>
+          <Button
+            onClick={() => onPageChange(pageIndex - 1)}
+            variant="outline"
+            className="h-8 w-8 p-0"
+            disabled={pageIndex===0}
+          >
+            <ChevronLeft className="h-4 w-4" />
+            <span className="sr-only">Página anterior</span>
+          </Button>
 
-            <Button variant="outline" className="h-8 w-8 p-0">
-                <ChevronRight className="h-4 w-4" />
-                <span className="sr-only">Próxima página</span>
-            </Button>
-            
-            <Button variant="outline" className="h-8 w-8 p-0">
-                <ChevronsRight className="h-4 w-4" />
-                <span className="sr-only">Última página</span>
-            </Button>
+          <Button
+            onClick={() => onPageChange(pageIndex + 1)}
+            variant="outline"
+            className="h-8 w-8 p-0"
+            disabled={pages <= pageIndex+1}
+          >
+            <ChevronRight className="h-4 w-4" />
+            <span className="sr-only">Próxima página</span>
+          </Button>
+
+          <Button
+            onClick={() => onPageChange(pages - 1)}
+            variant="outline"
+            className="h-8 w-8 p-0"
+            disabled={pages <= pageIndex+1}
+          >
+            <ChevronsRight className="h-4 w-4" />
+            <span className="sr-only">Última página</span>
+          </Button>
         </div>
       </div>
     </div>
--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,11 +1,25 @@
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { TableCell, TableRow } from "@/components/ui/table";
-import { DialogContent } from "@radix-ui/react-dialog";
 import { ArrowRight, Search, X } from "lucide-react";
 import { OrderDetails } from "./order-details";
+import { OrderStatus } from "@/components/order-status";
 
-export function OrderTableRow() {
+import {formatDistanceToNow} from 'date-fns'
+import {ptBR} from 'date-fns/locale'
+
+
+export interface OrderTableRowProps {
+  order: {
+    orderId: string;
+    createdAt: string;
+    status: "pending" | "canceled" | "processing" | "delivering" | "delivered";
+    customerName: string;
+    total: number;
+  };
+}
+
+export function OrderTableRow({ order }: OrderTableRowProps) {
   return (
     <TableRow>
       <TableCell>
@@ -21,14 +35,24 @@ export function OrderTableRow() {
         </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
-        df65s4f684sedgf8s
+        {order.orderId}
       </TableCell>
-      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
-      <TableCell className="">
-        
+      <TableCell className="text-muted-foreground">
+        {formatDistanceToNow(order.createdAt, {
+          locale: ptBR,
+          addSuffix: true
+        })}
       </TableCell>
-      <TableCell className="font-medium">Paulo Vinícius</TableCell>
-      <TableCell className="font-medium">R$ 149,90</TableCell>
+      <TableCell>
+        <OrderStatus status={order.status} />
+      </TableCell>
+      <TableCell className="font-medium">{order.customerName}</TableCell>
+      <TableCell className="font-medium">
+        {order.total.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        })}
+      </TableCell>
       <TableCell>
         <Button variant="outline" size="xs">
           <ArrowRight className="mr-2 h-3 w-3" />
